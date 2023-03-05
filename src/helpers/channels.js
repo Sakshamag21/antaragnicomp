9b61b72395d750d09411c91c190f27b8106a389c
Saksham agarwal <sakshamag21@iitk.ac.in>
2023-03-05 13:21:47 +0530

    timeline done

---
+++ src/helpers/channels.js
@@ -0,0 +1,254 @@
+export const channels = [
+    {
+        uuid: "16fdfefe-e466-4090-bc1a-57c43937f826",
+        type: "channel",
+        title: "r tv",
+        country: "USA",
+        provider: 7427,
+        logo:
+          "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/r-channel.png",
+        year: 2002
+      },
+  
+    {
+      uuid: "96aaf72c-b5ed-4ce4-937d-1912e4f8bf0d",
+      type: "channel",
+      title: "b tv",
+      country: "USA",
+      provider: 2764,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/b-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "06d3366b-9ec2-46e8-a741-df3ee1abeed7",
+      type: "channel",
+      title: "five tv",
+      country: "USA",
+      provider: 9749,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/five-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "b51689be-8eb4-4481-b208-6bbcb7fb47c2",
+      type: "channel",
+      title: "comedy tv",
+      country: "USA",
+      provider: 5066,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/c-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "b95dc0e8-93d6-473e-a0ce-839d988b1dcf",
+      type: "channel",
+      title: "tm tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/tm-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "359563a3-3bbb-40f7-8a55-bec3b11180c4",
+      type: "channel",
+      title: "w tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/w-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "21dda491-f640-4db5-9172-d35eedd06ea5",
+      type: "channel",
+      title: "six tv",
+      country: "USA",
+      provider: 703,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/six-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "d75873f4-4f2c-46f6-b6d6-65cfa62b8cf7",
+      type: "channel",
+      title: "nfi tv",
+      country: "USA",
+      provider: 8851,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/nfi-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "a7238d3d-f1e6-4934-a663-37c1902920cd",
+      type: "channel",
+      title: "dragon tv",
+      country: "USA",
+      provider: 4447,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/dragon-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "da5fc532-fa14-481b-91c7-0c8bc4ee91a3",
+      type: "channel",
+      title: "cd tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/cd-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "bbd592e8-bd97-4889-915d-3c4b14788cfb",
+      type: "channel",
+      title: "tennis tv",
+      country: "USA",
+      provider: 563,
+      logo:
+        "https://github.com/karolkozer/planby-demo-resources/blob/master/resources/channel-logos/png/tennis-ball-channel.png?raw=true",
+      year: 2002
+    },
+    {
+      uuid: "371ffac1-a5cd-4702-96b7-a35eba5edd7d",
+      type: "channel",
+      title: "bowling tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/bowling-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "834bed5f-5b88-46cd-bb04-0c2cd208bccb",
+      type: "channel",
+      title: "movie tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/movie-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "faf1be3a-ec39-43ab-86c4-37f3e59c2a7a",
+      type: "channel",
+      title: "r tv",
+      country: "USA",
+      provider: 7427,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/r-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "5b61a5f3-314f-4e8b-ae90-4fca1f75a322",
+      type: "channel",
+      title: "b tv",
+      country: "USA",
+      provider: 2764,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/b-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "eb202e9d-15ee-4bce-a1a8-31067e761a13",
+      type: "channel",
+      title: "five tv",
+      country: "USA",
+      provider: 9749,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/five-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "ea11bc6c-5108-4aab-b52e-37ac3ab7aec9",
+      type: "channel",
+      title: "comedy tv",
+      country: "USA",
+      provider: 5066,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/c-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "abb06f7d-cfc9-4b9e-9c4b-f68b49bb79f9",
+      type: "channel",
+      title: "tm tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/tm-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "f3cd7461-202c-41d3-858a-f5313c873ca6",
+      type: "channel",
+      title: "w tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/w-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "93fd7dc6-ef9c-41c8-bc6d-d2b99714a81c",
+      type: "channel",
+      title: "six tv",
+      country: "USA",
+      provider: 703,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/six-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "dda97845-3014-4076-a95a-45ace93066fa",
+      type: "channel",
+      title: "nfi tv",
+      country: "USA",
+      provider: 8851,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/nfi-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "160cf7f7-5362-4e60-b826-cc737bc33a64",
+      type: "channel",
+      title: "dragon tv",
+      country: "USA",
+      provider: 4447,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/dragon-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "827d2bd6-7ca2-4ace-a413-c4e0eecad5d6",
+      type: "channel",
+      title: "cd tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/cd-channel.png",
+      year: 2002
+    },
+    {
+      uuid: "1e04ef2d-b361-4869-b77a-17448579ee93",
+      type: "channel",
+      title: "tennis tv",
+      country: "USA",
+      provider: 563,
+      logo:
+        "https://github.com/karolkozer/planby-demo-resources/blob/master/resources/channel-logos/png/tennis-ball-channel.png?raw=true",
+      year: 2002
+    },
+    {
+      uuid: "7edbc4fb-c507-40cc-9247-7321ed8c7c63",
+      type: "channel",
+      title: "bowling tv",
+      country: "USA",
+      provider: 5380,
+      logo:
+        "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/bowling-channel.png",
+      year: 2002
+    },
+  ];
+  
---
+++ src/helpers/epg.js
@@ -0,0 +1,30 @@
+export const epg=[
+    {"id":"1",
+    "title":"The Book of Boba Fett",
+    "isYesterday":true,
+    "since":"2023-03-04T23:50:00",
+    "till":"2023-03-05T00:55:00",
+    "channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826",
+    "image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/sjx6zjQI2dLGtEL0HGWsnq6UyLU.jpg",},
+
+    {"id":"2","title":"And Just Like That...","since":"2023-03-05T00:55:00","till":"2023-03-05T02:35:00","channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826","image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/1CqkiWeuwlaMswG02q3mBmraiDM.jpg",},
+    {"id":"3","title":"Late Night with Conan O'Brien","since":"2023-03-05T02:35:00","till":"2023-03-05T03:30:00","channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826","image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/q9unI9oHyLUNK97RQPYAGjFs4Xu.jpg"},
+    {"id":"4",
+    "title":"The Book of Boba Fett",
+    "since":"2023-03-05T03:50:00",
+    "till":"2023-03-05T04:55:00",
+    "channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826",
+    "image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/sjx6zjQI2dLGtEL0HGWsnq6UyLU.jpg",},
+
+    {"id":"5","title":"And Just Like That...","since":"2023-03-05T05:55:00","till":"2023-03-05T06:35:00","channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826","image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/1CqkiWeuwlaMswG02q3mBmraiDM.jpg",},
+    {"id":"6","title":"Late Night with Conan O'Brien","since":"2023-03-05T07:35:00","till":"2023-03-05T08:30:00","channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826","image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/q9unI9oHyLUNK97RQPYAGjFs4Xu.jpg"},
+    {"id":"7",
+    "title":"The Book of Boba Fett",
+    
+    "since":"2023-03-05T09:50:00",
+    "till":"2023-03-05T10:55:00",
+    "channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826",
+    "image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/sjx6zjQI2dLGtEL0HGWsnq6UyLU.jpg",},
+
+    {"id":"8","title":"And Just Like That...","since":"2023-03-05T11:55:00","till":"2023-03-05T12:35:00","channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826","image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/1CqkiWeuwlaMswG02q3mBmraiDM.jpg",},
+    {"id":"9","title":"Late Night with Conan O'Brien","since":"2023-03-05T12:35:00","till":"2023-03-05T13:30:00","channelUuid":"16fdfefe-e466-4090-bc1a-57c43937f826","image":"https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/q9unI9oHyLUNK97RQPYAGjFs4Xu.jpg"},]
